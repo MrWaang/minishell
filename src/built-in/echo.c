d1f1a049c30de30fdacdcfc0524a0e5664a78d8c
tbosviel <theo1701@gmail.com>
2025-06-05 17:22:34 +0200

    norm echo

--- src/built-in/echo.c
+++ src/built-in/echo.c
@@ -12,41 +12,40 @@
 
 #include "../../includes/minishell.h"
 
-int ft_echo(char **args)
+int	ft_echo(char **args)
 {
-	int i;
-	int n_option;
+	int	i;
+	int	n_option;
 
 	i = 1;
 	n_option = 0;
-	while(args[i] && ft_n_option(args[i]))
+	while (args[i] && ft_n_option(args[i]))
 	{
 		n_option = 1;
 		i++;
 	}
-	while(args[i])
+	while (args[i])
 	{
 		printf("%s", args[i]);
-		if(args[i + 1])
+		if (args[i + 1])
 			printf(" ");
 		i++;
 	}
-	if(!n_option)
+	if (!n_option)
 		printf("\n");
 	return (0);
 }
 
-int ft_n_option(char *args)
+int	ft_n_option(char *args)
 {
-	int i;
+	int	i;
 
-	if(!args || args[0] != '-' || args[1] != 'n')
+	if (!args || args[0] != '-' || args[1] != 'n')
 		return (0);
-
 	i = 2;
-	while(args[i])
+	while (args[i])
 	{
-		if(args[i] != 'n')
+		if (args[i] != 'n')
 			return (0);
 		i++;
 	}
